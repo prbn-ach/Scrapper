a6d49bc4d3e9948fa90ffd61813659b4f9d5a05e
prbn-ach <prabin-acharya-2002@gmail.com>
2025-06-29 16:54:45 +0545

    Finish project

---
+++ scrapper.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import csv
+
+url = "http://books.toscrape.com"
+book_list=[]
+def scrapeBooks(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Failed to load page")
+        return []
+    
+    response.encoding = response.apparent_encoding
+    soup = BeautifulSoup(response.text,"html.parser")
+    books = soup.find_all("article", class_="product_pod")
+    for book in books:
+        title = book.h3.a['title']
+        price_text= book.find('p',class_='price_color').text
+        currency = price_text[0]
+        price = float(price_text[1:]) 
+        book_list.append({
+            "title":title,
+            "currency":currency,
+            "price":price
+        })
+    return book_list
+all_books = scrapeBooks(url)
+with open ("book_list.json",'w',encoding="UTF-8") as f:
+    json.dump(all_books,f,indent=4,ensure_ascii=False)
+
+with open ("book_list.csv",'w',encoding="UTF-8") as c:
+    all_books_csv = csv.DictWriter(c,fieldnames=["title","currency","price"])
+    all_books_csv.writeheader()
+    all_books_csv.writerows(all_books)
