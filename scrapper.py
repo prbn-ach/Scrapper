a2d886caca9ec2b694928647a44fb35320180885
prbn-ach <prabin-acharya-2002@gmail.com>
2025-06-29 17:19:55 +0545

    arranged the imports

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
 import json
-
 import csv
+
+from bs4 import BeautifulSoup
+
 url = "http://books.toscrape.com"
-book_list=[]
+
+
+
 def scrapeBooks(url):
     response = requests.get(url)
     if response.status_code != 200:
@@ -14,6 +17,8 @@ def scrapeBooks(url):
     response.encoding = response.apparent_encoding
     soup = BeautifulSoup(response.text,"html.parser")
     books = soup.find_all("article", class_="product_pod")
+    
+    book_list=[]
     for book in books:
         title = book.h3.a['title']
         price_text= book.find('p',class_='price_color').text
@@ -25,6 +30,7 @@ def scrapeBooks(url):
             "price":price
         })
     return book_list
+
 all_books = scrapeBooks(url)
 with open ("book_list.json",'w',encoding="UTF-8") as f:
     json.dump(all_books,f,indent=4,ensure_ascii=False)
